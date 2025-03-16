9513f8bad6b9bd8c8345edc6ba17030dd6587e67
Abbadohdev <abbadohmedia@gmail.com>
2025-03-16 03:21:11 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+  
+    <!-- <link rel="icon" href="/templates/protostar/favicon.ico" type="image/x-icon"> -->
+    <link rel="shortcut icon" href="/favicon.ico">
+
+    <!--StyleSheet-->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/styles.css" type="text/css" />
+
+
+    <!--Javascript-->
+   <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
+
+</head>
+<body>
+  
+</body>
+</html>
