ea8b65831bd742405d0d8b6b1095f866d3903006
aidabehdar <aida.behdar74@gmail.com>
2025-06-09 14:56:46 +0330

    Create file.d
    Update portfolio image section

---
+++ aidabehdar/file.d
@@ -0,0 +1,29 @@
+<section id="projects">
+  <h2>Projects</h2>
+  <div class="gallery">
+    <a href="images/hendourabi.jpg" data-lightbox="portfolio" data-title="Hendourabi Multi Port">
+      <img src="images/hendourabi.jpg" alt="Hendourabi Multi Port">
+    </a>
+    <a href="images/abnoos.jpg" data-lightbox="portfolio" data-title="Abnoos Residence">
+      <img src="images/abnoos.jpg" alt="Abnoos Residence">
+    </a>
+    <a href="images/serenity.jpg" data-lightbox="portfolio" data-title="Serenity in Stone - Bathroom Visualization">
+      <img src="images/serenity.jpg" alt="Serenity in Stone">
+    </a>
+    <a href="images/kitchen.jpg" data-lightbox="portfolio" data-title="Modern Heritage Kitchen">
+      <img src="images/kitchen.jpg" alt="Modern Kitchen">
+    </a>
+    <a href="images/bathroom.jpg" data-lightbox="portfolio" data-title="Modern Bathroom Visualization">
+      <img src="images/bathroom.jpg" alt="Modern Bathroom">
+    </a>
+    <a href="images/slab.jpg" data-lightbox="portfolio" data-title="Architectural Slab Design">
+      <img src="images/slab.jpg" alt="Architectural Slab">
+    </a>
+    <a href="images/patina1.jpg" data-lightbox="portfolio" data-title="Ceiling Mural with Silver Leaf">
+      <img src="images/patina1.jpg" alt="Patina Ceiling 1">
+    </a>
+    <a href="images/patina2.jpg" data-lightbox="portfolio" data-title="Cloud-effect Patina with Oil Painting">
+      <img src="images/patina2.jpg" alt="Patina Ceiling 2">
+    </a>
+  </div>
+</section>
